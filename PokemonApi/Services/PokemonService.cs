8d072a49d4e8def6f571df3a6d1f4e81e676ad01
Italo Guerrieri <italo_guerrieri@hotmail.com>
2021-04-06 22:27:44 +0200

    Added some logging

--- PokemonApi/Services/PokemonService.cs
+++ PokemonApi/Services/PokemonService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PokemonApi.Data;
 
 namespace PokemonApi.Services
@@ -9,10 +11,12 @@ namespace PokemonApi.Services
     public class PokemonService : ControllerBase
     {
         private readonly IPokemonRepository _pokemonRepository;
+        private readonly ILogger<PokemonService> _logger;
 
-        public PokemonService(IPokemonRepository pokemonRepository)
+        public PokemonService(IPokemonRepository pokemonRepository, ILogger<PokemonService> logger)
         {
             _pokemonRepository = pokemonRepository;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -21,14 +25,23 @@ namespace PokemonApi.Services
         {
             try
             {
-                return ToPokemon(_pokemonRepository.Get(name));
+                var sw = AStartedStopwatch();
+                _logger.LogInformation($"{nameof(GetPokemon)} invoked, pokemon name: {name}");
+
+                var result = ToPokemon(_pokemonRepository.Get(name));
+
+                _logger.LogInformation($"{nameof(GetPokemon)} completed, elapsed time: {sw.ElapsedMilliseconds}ms");
+
+                return result;
             }
-            catch (ArgumentOutOfRangeException)
+            catch (ArgumentOutOfRangeException ex)
             {
+                _logger.LogError(ex, "No pokemon found with the given name");
                 return NotFound();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "An unexpected error occurred while retrieving pokemon information");
                 return StatusCode(500);
             }
         }
@@ -39,14 +52,23 @@ namespace PokemonApi.Services
         {
             try
             {
-                return ToTranslatedPokemon(_pokemonRepository.Get(name));
+                var sw = AStartedStopwatch();
+                _logger.LogInformation($"{nameof(GetPokemonTranslated)} invoked, pokemon name: {name}");
+
+                var result = ToTranslatedPokemon(_pokemonRepository.Get(name));
+
+                _logger.LogInformation($"{nameof(GetPokemonTranslated)} completed, elapsed time: {sw.ElapsedMilliseconds}ms");
+
+                return result;
             }
-            catch (ArgumentOutOfRangeException)
+            catch (ArgumentOutOfRangeException ex)
             {
+                _logger.LogError(ex, "No pokemon found with the given name");
                 return NotFound();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "An unexpected error occurred while retrieving pokemon information");
                 return StatusCode(500);
             }
         }
@@ -64,19 +86,28 @@ namespace PokemonApi.Services
             };
         }
 
-        private static Pokemon ToTranslatedPokemon(IPokemon pokemon)
+        private Pokemon ToTranslatedPokemon(IPokemon pokemon)
         {
             var result = ToPokemon(pokemon);
             try
             {
                 result.Description = pokemon.TranslatedDescription;
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogWarning(ex, "An unexpected error occurred while retrieving translated pokemon description");
                 // ignore
             }
 
             return result;
+        }
+
+        private static Stopwatch AStartedStopwatch()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            return sw;
         }
 
         #endregion
